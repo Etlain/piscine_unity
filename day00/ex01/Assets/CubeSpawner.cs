fdfafdbc38cfa9670814a94d9b333f4818ecb2d9
Mounir Mouhssine <Mounir@MacBook-Pro-de-Mounir.local>
2018-06-28 00:14:38 +0200

    game almost finished, missing precision of key

--- day00/ex01/Assets/CubeSpawner.cs
+++ day00/ex01/Assets/CubeSpawner.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour {
 
+	public GameObject	a;
+	public GameObject	s;
+	public GameObject	d;
+
+	[HideInInspector]
+	private GameObject	key;
+	private float		SpawnElapsedTime = 0;
+	private float		SpawnLimitTime = 0;
+	private float		SpawnMaxTime = 5;
+	private	int 		random_asd = -1;
+	private int 		asd_prev = -1;
 	// Use this for initialization
 	void Start () {
-		
+		initVar();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		SpawnElapsedTime += Time.deltaTime;
+		if (SpawnElapsedTime >= SpawnLimitTime)
+		{
+
+			if (random_asd == 0)
+				key = GameObject.Instantiate(a);
+			else if (random_asd == 1)
+				key = GameObject.Instantiate(s);
+			else if (random_asd == 2)
+				key = GameObject.Instantiate(d);
+			SpawnElapsedTime -= SpawnLimitTime;
+			initVar();
+		}
+
+		//key.transform.Translate(Vector3.down * 5 * Time.deltaTime);
+	}
+
+	void initVar()
+	{
+		SpawnLimitTime  = Random.Range(1, SpawnMaxTime + 1);
+		while (random_asd == asd_prev)
+				random_asd = Random.Range(0, 3);
+		asd_prev = random_asd;
+		Debug.Log(random_asd);
 	}
 }
