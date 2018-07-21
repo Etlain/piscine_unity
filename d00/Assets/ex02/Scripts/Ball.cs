6747d2f33af9865ff406bb26e8c90814d58b44c8
Mounir Mouhssine <Mounir@MacBook-Pro-de-Mounir.local>
2018-07-21 21:33:12 +0200

    add score, and ball in hole ok

--- d00/Assets/ex02/Scripts/Ball.cs
+++ d00/Assets/ex02/Scripts/Ball.cs
@@ -9,25 +9,29 @@ public class Ball : MonoBehaviour {
 	public GameObject down_border;
 	public GameObject hole;
 
+	[HideInInspector]
+
 	bool start_speed_ball = false;
 	float ball_speed = 0;
 	bool direction_up = true;
+	private GameObject ball;
+	bool game = true;
 
 	// Use this for initialization
 	void Start () {
-
+		ball = this.gameObject;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (club.move_ball == true)
+		if (club.move_ball)
 		{
 			if (!start_speed_ball)
 			{
-				ball_speed = club.force_distance * (10.0f);
+				ball_speed = club.force_distance * (100.0f);
 				if (ball_speed < 0)
 					ball_speed = ball_speed * -1;
-				start_speed_ball = true;
+				//start_speed_ball = true; // modifier plus bas pour gerer le score
 				if (!club.ball_up_hole)
 					direction_up = true;
 				else
@@ -36,47 +40,68 @@ public class Ball : MonoBehaviour {
 			float ball_distance = ball_speed * Time.deltaTime;
 			if (direction_up)
 			{
-				if (up_border.transform.localPosition.y > ball_distance + this.gameObject.transform.localPosition.y)
-					this.gameObject.transform.Translate(Vector3.up * ball_distance);
+				if (ball_speed < 10.0f && ball.transform.localPosition.y <= hole.transform.localPosition.y && ball.transform.localPosition.y > hole.transform.localPosition.y - 0.7)
+				{
+					ball.transform.localScale = new Vector3 (0, 0, 0);
+					ball_speed = 0;
+					if (club.score <= 0)
+						Debug.Log("Victory :)");
+					club.score-=5;
+					game = false;
+				}
+				else if (up_border.transform.localPosition.y > ball_distance + ball.transform.localPosition.y)
+					ball.transform.Translate(Vector3.up * ball_distance);
 				else
 				{
-					this.gameObject.transform.Translate(Vector3.up * (up_border.transform.localPosition.y - this.gameObject.transform.localPosition.y));
+					ball.transform.Translate(Vector3.up * (up_border.transform.localPosition.y - ball.transform.localPosition.y));
 					direction_up = false;
 				}
 			}
 			else
 			{
-				if (down_border.transform.localPosition.y < this.gameObject.transform.localPosition.y - ball_distance)
-					this.gameObject.transform.Translate(Vector3.down * ball_distance);
+				if (ball_speed < 10.0f && ball.transform.localPosition.y <= hole.transform.localPosition.y + 0.7 && ball.transform.localPosition.y > hole.transform.localPosition.y)
+				{
+					ball.transform.localScale = new Vector3 (0, 0, 0);
+					ball_speed = 0;
+					if (club.score <= 0)
+						Debug.Log("Victory :)");
+					club.score-=5;
+					game = false;
+				}
+				/*else if (ball_speed < 15.0f && ball.transform.localPosition.y < hole.transform.localPosition.y + 0.5 && ball.transform.localPosition.y > hole.transform.localPosition.y - 0.5)
+					ball_speed = 0;*/
+				else if (down_border.transform.localPosition.y < ball.transform.localPosition.y - ball_distance)
+					ball.transform.Translate(Vector3.down * ball_distance);
 				else
 				{
-					this.gameObject.transform.Translate(Vector3.down * (this.gameObject.transform.localPosition.y - down_border.transform.localPosition.y));
+					ball.transform.Translate(Vector3.down * (ball.transform.localPosition.y - down_border.transform.localPosition.y));
 					direction_up = true;
 				}
-				//this.gameObject.transform.Translate(Vector3.down * ball_distance);
 			}
-
-			//print("before ball_speed : "+ball_speed);
+			if (!start_speed_ball)
+			{
+				club.score+=5;
+				if (club.score > 0 && game)
+				{
+					Debug.Log("Defeat :(");
+					game = false;
+				}
+				Debug.Log("Score : "+club.score);
+				start_speed_ball = true;
+			}
 			ball_speed = ball_speed - 1.0f;
-			//club.transform.localPosition = new Vector3 (this.gameObject.localPosition.x, 0, 0);
-			//print("after ball_speed : "+ball_speed);
 			if (ball_speed < 0)
 			{
-				print("finish");
+				//print("finish");
 				club.force_distance = 0;
 				club.move_ball = false;
 				start_speed_ball = false;
+				// placer le club correctement
 			}
-			if (this.gameObject.transform.localPosition.y > hole.transform.localPosition.y)
+			if (ball.transform.localPosition.y > hole.transform.localPosition.y)
 				club.ball_up_hole = true;
 			else
 				club.ball_up_hole = false;
-			//club.force_distance = 0;
-			//club.move_ball = false;
-
-
-			//if ()
-			//print(club.force_distance);
 		}
 	}
 }
--- d00/Assets/ex02/Scripts/Club.cs
+++ d00/Assets/ex02/Scripts/Club.cs
@@ -9,8 +9,9 @@ public class Club : MonoBehaviour {
 	[HideInInspector]
 	public float force_distance = 0;
 	public bool move_ball = false;
-	bool hit = false;
+	public bool hit = false;
 	public bool ball_up_hole = false;
+	public int score = -15;
 
 
 	void Start () {
@@ -32,9 +33,7 @@ public class Club : MonoBehaviour {
 					this.gameObject.transform.Translate(Vector3.up * 3 * Time.deltaTime);
 					force_distance += 1 * 3 * Time.deltaTime;
 				}
-				//force_distance += -1 * 3 * Time.deltaTime;
 				hit = true;
-				//print("v down : "+ (Vector3.down * 3)+"time delta time : "+Time.deltaTime);
 			}
 			else if (hit == true)
 			{
@@ -42,9 +41,7 @@ public class Club : MonoBehaviour {
 					this.gameObject.transform.Translate(Vector3.up * (-force_distance));
 				else
 					this.gameObject.transform.Translate(Vector3.down * (-force_distance));
-				//force_distance = 0;
 				hit = false;
-				print (force_distance);
 				move_ball = true;
 			}
 		}
