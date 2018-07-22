721c6ec9476535d0344c06ea22849b9083d4ee06
Mounir Mouhssine <Mounir@MacBook-Pro-de-Mounir.local>
2018-07-22 23:39:49 +0200

    ex03 mouvement ok, pipes mouvement ok

--- d00/Assets/ex03/Scripts/Bird.cs
+++ d00/Assets/ex03/Scripts/Bird.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour {
 
+	[HideInInspector]
+	private GameObject	bird;
+	private float		old_Y;
+	private float		old_time = 0;
+
 	// Use this for initialization
 	void Start () {
-		
+		bird = this.gameObject;
+		old_Y = bird.transform.position.y;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		// gestion mouvement bird space sur l axe Y
+		if (Input.GetKeyDown("space")){
+			old_time = Time.fixedTime;
+		}
+		float y;
+		float t = 4 * (Time.fixedTime - old_time);
+		y = -4f * t + 8f;
+		//bird.transform.Rotate(Vector3.forward * Time.deltaTime /** 10 * y*//*);
+		//print("y : "+y+" time : "+t);
+		bird.transform.Translate(Vector3.up * y * Time.deltaTime);
 	}
 }
--- d00/Assets/ex03/Scripts/Pipe.cs
+++ d00/Assets/ex03/Scripts/Pipe.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour {
 
+	public GameObject bird;
+
+	[HideInInspector]
+	private GameObject pipe;
+
 	// Use this for initialization
 	void Start () {
-		
+		pipe = this.gameObject;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+		// mouvement pipe
+		/*pipe.transform.Translate(Vector3.left * Time.deltaTime); // -7, 7
+		//print("camera :"+Camera.main.rect.x);
+		//print("pipe pos: "+pipe.transform.position.x);
+		if (pipe.transform.position.x <= -7)
+			pipe.transform.Translate(Vector3.right * 14);
+		else if (bird.transform.position.x <= pipe.transform.position.x + 1 && bird.transform.position.x >= pipe.transform.position.x - 1)
+		{
+			if (bird.transform.position.y <= pipe.transform.position.y + 0.5 && bird.transform.position.y >= pipe.transform.position.y - 0.5)
+			{
+				print("bird in pipe");
+			}
+			//print("bird with pipe");
+		}
+		else
+			print("finish");*/
+		//	print("i m visible");
+		//if (pipe.transform.localPosition.x < )
+		// gestion collision avec l'oiseau
 	}
+
+	void OnBecameInvisible()
+    {
+        //enabled = false;
+    }
+
 }
