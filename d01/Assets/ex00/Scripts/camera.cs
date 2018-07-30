ef9eec649cc681c08185235b92792e07eeef592d
Mounir Mouhssine <Mounir@MacBook-Pro-de-Mounir.local>
2018-07-30 17:40:07 +0200

    day01, ex00 finished

--- d01/Assets/ex00/Scripts/camera.cs
+++ d01/Assets/ex00/Scripts/camera.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class camera : MonoBehaviour {
 
 	// Use this for initialization
+
+//	public GameObject player;
+
+	[HideInInspector]
+	//private Vector3 playerPosition = new Vector3(0, 0, 0);
+	private GameObject player = null;
+	private Vector3 offset = new Vector3(0, 0, 0);
+
 	void Start () {
-		
+		//offset = this.transform.position - player.transform.position;
 	}
-	
-	// Update is called once per frame
+
+	// LateUpdate is called once per frame
 	void Update () {
-		
+		if (player)
+			this.transform.position = player.transform.position + offset;
+		//print("pos cam :"+);
+	}
+
+	public void setOffset(GameObject player)
+	{
+		this.player = player;
+		this.transform.position = player.transform.position + offset + new Vector3(0, 0, this.transform.position.z);
+		offset = this.transform.position - player.transform.position;
+		//print("offset : "+pl);
+		//player = player;
 	}
 }
--- d01/Assets/ex00/Scripts/playerScript_ex00.cs
+++ d01/Assets/ex00/Scripts/playerScript_ex00.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerScript_ex00 : MonoBehaviour {
 
-	public int speed = 2;
-	public int jump = 10;
+	public camera	cam;
+	public int		speed = 2;
+	public int		jump = 10;
 
 	[HideInInspector]
 	private playerScript_ex00 player;
 	private bool 			  b_jump = false;
 	private static int 		  selected_player = 0;
 	private int 			  id_player = 1;
+	private bool			  b_camera = true;
 	//private static int 		  selected_player2 = 1;
 
 
@@ -26,7 +29,7 @@ public class playerScript_ex00 : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		print("selected_player : "+selected_player+"id_player : "+id_player);
+		//print("selected_player : "+selected_player+"id_player : "+id_player);
 		if (selected_player == id_player)
 		{
 			if (Input.GetKey("left"))
@@ -39,18 +42,41 @@ public class playerScript_ex00 : MonoBehaviour {
 				player.GetComponent<Rigidbody2D>().AddForce(Vector3.up * jump, ForceMode2D.Impulse);
 				b_jump = true;
 			}
+
+			// modifier personnage suivie par la camera
+			if (b_camera)
+			{
+				cam.setOffset(this.gameObject);
+				b_camera = false;
+			}
+		}
+		else if (Input.GetKey("r"))
+		{
+			selected_player = 0;
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 		}
 		else if (Input.GetKey("a"))
+		{
 			selected_player = 1;
+			b_camera = true;
+		}
 		else if (Input.GetKey("z"))
+		{
 			selected_player = 2;
+			b_camera = true;
+		}
 		else if (Input.GetKey("e"))
+		{
 			selected_player = 3;
+			b_camera = true;
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.gameObject.tag == "Ground")
+			b_jump = false;
+		else if (collision.gameObject.tag == "Player")
 			b_jump = false;
 	}
 }
