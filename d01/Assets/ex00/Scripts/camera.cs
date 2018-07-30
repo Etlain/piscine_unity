019d1e5fbf2a7ae2da3f1c1de68639404cd1d4d8
Mounir Mouhssine <Mounir@MacBook-Pro-de-Mounir.local>
2018-07-30 18:42:50 +0200

    correction val random

--- d01/Assets/ex00/Scripts/camera.cs
+++ d01/Assets/ex00/Scripts/camera.cs
@@ -14,22 +14,18 @@ public class camera : MonoBehaviour {
 	private Vector3 offset = new Vector3(0, 0, 0);
 
 	void Start () {
-		//offset = this.transform.position - player.transform.position;
 	}
 
 	// LateUpdate is called once per frame
 	void Update () {
 		if (player)
 			this.transform.position = player.transform.position + offset;
-		//print("pos cam :"+);
 	}
 
 	public void setOffset(GameObject player)
 	{
 		this.player = player;
-		this.transform.position = player.transform.position + offset + new Vector3(0, 0, this.transform.position.z);
+		this.transform.position = player.transform.position + new Vector3(0, 0, this.transform.position.z);
 		offset = this.transform.position - player.transform.position;
-		//print("offset : "+pl);
-		//player = player;
 	}
 }
