1608f78131ba22b33b72fd34e7158f1063153c77
Mounir MOUHSSINE <mmouhssi@e2r2p17.42.fr>
2018-08-09 15:11:00 +0200

    create script for teleport finished

---
+++ d01/Assets/ex03/Scripts/Teleport.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+
+	public int		idTeleport = 0;
+	public bool		isIn = false;
+
+	private GameObject teleport;
+	private static bool	isTeleport = false;
+	private static GameObject player = null;
+
+	// Use this for initialization
+	void Start () {
+		teleport = this.gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isTeleport && !isIn && player && this.tag == "TeleportOut")
+		{
+			player.transform.position = teleport.transform.position;
+			isTeleport = false;
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (this.tag == "TeleportIn")
+		{
+			player = collider.transform.gameObject;
+			isTeleport = true;
+		}
+	}
+
+}
