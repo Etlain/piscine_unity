f4adbd3d494f7fb5606dbc2d2773ab6249cb2fc9
Mounir MOUHSSINE <mmouhssi@e2r4p18.42.fr>
2018-08-16 21:12:09 +0200

    first door in work

---
+++ d01/Assets/ex04/Scripts/Button.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	public GameObject door;
+
+	private GameObject button;
+	// Use this for initialization
+	void Start () {
+		button = this.gameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		print(door.tag);
+		if (collision.gameObject.tag == "Thomas" && door.tag == "RedDoor")
+			OpenDoor();
+		else if (collision.gameObject.tag == "John" && door.tag == "YellowDoor")
+			OpenDoor();
+		else if (collision.gameObject.tag == "Claire" && door.tag == "BlueDoor")
+			OpenDoor();
+	}
+
+	void OpenDoor()
+	{
+		button.transform.localScale = new Vector3 (0, 0, 0);
+		door.transform.localScale = new Vector3 (0, 0, 0);
+	}
+}
