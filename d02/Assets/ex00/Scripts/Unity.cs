d67940285ee66c4f731c41a1479089d6e42cf2c8
Etlain <mounir92600@hotmail.fr>
2019-09-24 23:50:23 +0200

    unity test modification script for animations and angle 360d

--- d02/Assets/ex00/Scripts/Unity.cs
+++ d02/Assets/ex00/Scripts/Unity.cs
@@ -15,6 +15,7 @@ public class Unity : MonoBehaviour
     private float           step;
     private float           distanceX = 1;
     private float           distanceY = 0;
+    private float           angleUnityTarget = 0;
 
     private bool isSelected = false;
     private bool isClickTarget = false;
@@ -43,18 +44,19 @@ public class Unity : MonoBehaviour
 
     void moveToTarget()
     {
+
         step = speed * Time.deltaTime;
         unity.transform.position = Vector3.MoveTowards(unity.transform.position, clickTarget, step);
         //unityAnimator.setFloat("Horizontal", "");
         //Debug.Log("target pos :"+clickTarget);
         //Debug.Log("unity pos :"+unity.transform.position);
 
-        if (Vector3.Distance(unity.transform.position, clickTarget) < step + 0.1)
+        if (Vector3.Distance(unity.transform.position, clickTarget) < step)
         {
             unityAnimator.SetFloat("Speed", 0);
             //if (Mathf.Abs(distanceX) > )
-            unityAnimator.SetFloat("Horizontal", distanceX);
-            unityAnimator.SetFloat("Vertical", distanceY);
+            //unityAnimator.SetFloat("Horizontal", distanceX);
+            //unityAnimator.SetFloat("Vertical", distanceY);
             isClickTarget = false;
         }
         else
@@ -63,13 +65,47 @@ public class Unity : MonoBehaviour
             distanceY = clickTarget.y - unity.transform.position.y;
 
             //if (Mathf.Abs(distanceX) > 0.1)
-                unityAnimator.SetFloat("Horizontal", distanceX);
+            //angleUnityTarget = Vector2.SignedAngle(clickTarget, unity.transform.position);
+            angleUnityTarget = SignedAngle(unity.transform.position, clickTarget, Vector2.right);
+
+            float angleOffset = 20;
+            // LEFT
+            if (angleUnityTarget >= 180 - angleOffset || angleUnityTarget <= -180 + angleOffset)
+            {
+                unityAnimator.SetFloat("Horizontal", -1);
+                unityAnimator.SetFloat("Vertical", 0);
+            }
+            // UP
+            else if (angleUnityTarget >= 90 - angleOffset || angleUnityTarget <= 90 + angleOffset)
+            {
+                unityAnimator.SetFloat("Horizontal", 0);
+                unityAnimator.SetFloat("Vertical", 1);
+            }
+            else
+            {
+                unityAnimator.SetFloat("Horizontal", 0);
+                unityAnimator.SetFloat("Vertical", 0);
+            }
+            //else if (angleUnityTarget >= 90 - angleOffset || angleUnityTarget <= 90 + angleOffset)
+            // 180 - 90 upleft
+            // 180 left
+            // 90 up
+
+            //if ()
+            //    unityAnimator.SetFloat("Horizontal", distanceX);
             //if (Mathf.Abs(distanceY) > 0.1)
-                unityAnimator.SetFloat("Vertical", distanceY);
+            //    unityAnimator.SetFloat("Vertical", distanceY);
             unityAnimator.SetFloat("Speed", 1);
         }
-        Debug.Log("distanceX :"+distanceX);
-        Debug.Log("distanceY :"+distanceY);
+        Debug.Log("distanceX :"+distanceX+", distanceY :"+distanceY+", angle :"+angleUnityTarget);
+    }
+
+    public static float SignedAngle( Vector3 origin, Vector3 to, Vector3 normal )
+    {
+        // angle in [0,180]
+        float angle = Vector3.Angle( origin, to );
+        float sign = Mathf.Sign( Vector3.Dot( normal, Vector3.Cross( origin, to ) ) );
+        return angle * sign;
     }
 
     void getClickTarget()
